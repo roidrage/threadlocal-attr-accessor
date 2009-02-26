86d445fffc79e6a242d688a0ddfee6beffe524c8
Mathias Meyer <meyer@paperplanes.de>
2009-02-26 16:35:19 +0100

    Minor cleanup.

--- lib/core_ext/module.rb
+++ lib/core_ext/module.rb
@@ -1,14 +1,7 @@
 class Module
   def tl_attr_accessor(*names)
-    names.each do |name|
-      define_method(name) do
-        Thread.current["#{self.class.name.downcase}_#{object_id}_#{name.to_s}"]
-      end
-
-      define_method("#{name}=".to_sym) do |new_value|
-        Thread.current["#{self.class.name.downcase}_#{object_id}_#{name.to_s}"] = new_value
-      end
-    end
+    tl_attr_reader(*names)
+    tl_attr_writer(*names)
   end
   
   def tl_attr_reader(*names)
